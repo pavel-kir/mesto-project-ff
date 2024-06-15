97f5a2b2a9fb1c06e3a06ecdc9fd9478245f3355
Pavel Kirsanov <kirsanov.p.v@yandex.ru>
2024-06-15 23:29:02 +0300

    Создавать и удалять карточки с помощью JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(data, funDelete) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = data.link;
+  card.querySelector('.card__title').textContent = data.name;
+  card.querySelector('.card__delete-button').addEventListener('click', funDelete);
+  return card;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+  const listItem = evt.target.closest('.card');
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(addCard(item, removeCard));
+});
